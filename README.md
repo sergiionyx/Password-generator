37ad838f3d3dff53f7e61989e0e07e39ae8debf3
sergii-onyx <twilight.cympak@gmail.com>
2022-02-17 18:33:54 -0500

    password generator completed

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Password-generator
+Generator of random password based on chosen 
+
+## Website
+https://sergiionyx.github.io/Password-generator/
+
+Or check the code here: https://github.com/sergiionyx/Password-generator
+
+## Purpose
+A website created to generate a password based on chosen criterias:
+
+- optional lenght (8-128 char's);
+- can include one or all given options (lower case, upper case, numbers, special characters);
+- all characters in password is randomised;
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Contribution
+Made with ❤️ by Sergii Onyshchenko
+
+## Screenshot of website
+![Example](./assets/screenshots/main-page-example.jpg)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,63 @@
 // Assignment code here
+var AskPassLength;
+var CheckPassLength;
+var password = "";
+var passArray = [];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specCharsArray =   ["\[", "\!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "\-", "=", "\[", "\]", "{", "}", ";", '\'', ":", '"', "\\", "|", ",", ".", "<", ">","\/", "?", "\]", "+", "\;"];
+
+var PassLength = function () {
+  passArray = [];
+  AskPassLength = prompt("Please enter your password length (from 8 to 128characters).");
+  CheckPassLength = parseInt(AskPassLength);
+  if(CheckPassLength < 8 || CheckPassLength > 128) {
+    PassLength();
+  }
+  else if (!CheckPassLength) {
+    return PassLength();
+  }
+
+  var confirmLowerCase = window.confirm("Do you want to include Lower Case characters?");
+  var confirmUpperCase = window.confirm("Do you want to include Upper Case characters?");
+  var confirmNumb = window.confirm("Do you want to include numbers?");
+  var confirmSpecChar = window.confirm("Do you want to include special characters?");
+  if (!confirmLowerCase && !confirmUpperCase && !confirmNumb && !confirmSpecChar) {
+    window.alert("You need to confirm at least 1 option")
+    PassLength();
+  }
+  if (confirmLowerCase) {
+    passArray.push.apply(passArray, lowerCaseArray);
+  }
+  
+  if(confirmUpperCase) {
+    passArray.push.apply(passArray, upperCaseArray);
+  }
+  
+  if(confirmNumb) {
+    passArray.push.apply(passArray, numbersArray);
+  }
+  
+  if(confirmSpecChar) {
+    passArray.push.apply(passArray, specCharsArray);
+  }
+}
+
+function generatePassword() {
+  password = "";
+  PassLength();
+  for ( i=0; i < CheckPassLength; i++) {
+    var randomChar = passArray[Math.floor(Math.random()*passArray.length)];
+    password = password.concat(randomChar);
+    }
+    
+  return password;
+}
+
+
+
+
 
 
 // Get references to the #generate element
@@ -15,3 +74,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
